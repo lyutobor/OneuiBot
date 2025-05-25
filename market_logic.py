979313f081b77e33ceb7041c891a499a1adbccf5
lyutobor <53938379+lyutobor@users.noreply.github.com>
2025-05-25 19:02:39 +0200

    market_logic.py

--- market_logic.py
+++ market_logic.py
@@ -153,13 +153,13 @@ async def cmd_market_buy_item(message: Message, command: CommandObject, state: F
         original_price_display_if_discounted = 0 # Будет использоваться, если применяется скидка, но по умолчанию это будет динамическая цена
         
         if item_key_to_buy == "oneui_attempt":
-            final_total_price_to_charge = int(max(1, current_balance * Config.MARKET_ONEUI_ATTEMPT_PERCENT_OF_BALANCE))
-            original_price_display_if_discounted = final_total_price_to_charge # Для динамической цены это и есть "оригинальная" до скидок чехла
+            final_total_price_to_charge = int(max(Config.MIN_MARKET_DYNAMIC_PRICE, current_balance * Config.MARKET_ONEUI_ATTEMPT_PERCENT_OF_BALANCE)) # <--- ИЗМЕНЕНИЕ ЗДЕСЬ
+            original_price_display_if_discounted = final_total_price_to_charge 
         elif item_key_to_buy == "roulette_spin":
-            final_total_price_to_charge = int(max(1, current_balance * Config.MARKET_ROULETTE_SPIN_PERCENT_OF_BALANCE))
+            final_total_price_to_charge = int(max(Config.MIN_MARKET_DYNAMIC_PRICE, current_balance * Config.MARKET_ROULETTE_SPIN_PERCENT_OF_BALANCE)) # <--- ИЗМЕНЕНИЕ ЗДЕСЬ
             original_price_display_if_discounted = final_total_price_to_charge
         elif item_key_to_buy == "bonus_attempt":
-            final_total_price_to_charge = int(max(1, current_balance * Config.MARKET_BONUS_ATTEMPT_PERCENT_OF_BALANCE))
+            final_total_price_to_charge = int(max(Config.MIN_MARKET_DYNAMIC_PRICE, current_balance * Config.MARKET_BONUS_ATTEMPT_PERCENT_OF_BALANCE)) # <--- ИЗМЕНЕНИЕ ЗДЕСЬ
             original_price_display_if_discounted = final_total_price_to_charge
         else:
             # Для остальных товаров (если будут), используем старую логику с инфляцией
